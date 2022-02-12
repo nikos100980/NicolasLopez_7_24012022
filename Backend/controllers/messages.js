32fe175b435513a707d5b807f05a26e58813a8b5
LOPEZ Nicolas <nikos100980@free.fr>
2022-02-12 01:49:24 +0100

    Création des modules pour messages

--- Backend/controllers/messages.js
+++ Backend/controllers/messages.js
@@ -1,35 +1,57 @@
 // importation des modules necessaires
 
-const models = require('../models');
-const cookies = require('../middleware/authmessage');
-const fs = require('fs');
-
+const models = require("../models");
+const cookies = require("../middleware/authmessage");
+const fs = require("fs");
 
 // Ajout du module pour la creation d'un message
 
-exports.createMessage = async (req,res,next)=>{
-    const userId = cookies.getUserId(req);
-    let imageUrl;
-    try {
-        const userFound = await models.User.findOne({
-            attributes: ['firstname','lastname','picture'],
-            where:{ id: userId},
-        });
-        if(userFound){
-            if (req.file){
-                imageUrl = `${req.protocol}://${req.get("host")}/images/${
-                    req.file.filename
-                  }`;
-            }else{
-                imageUrl = null;
-            }
-            const message = await models.Message.create({
-                content: content,
-                
-            })
-        }
-    } catch (error) {
-        
-    }
+exports.createMessage = async (req, res, next) => {
+  const userId = cookies.getUserId(req);
 
+  const content = req.body.content;
+  const attachment = req.body.attachment;
+  let imageUrl;
+
+  try {
+    await models.User.findOne({
+      where: { id: userId },
+    })
+      .then( async (userFound) => {
+        if (userFound) {
+          if (req.file) {
+            imageUrl = `${req.protocol}://${req.get("host")}/images/${
+              req.file.filename
+            }`;
+          } else {
+            imageUrl = null;
+          };
+
+          const message = await models.Message.create({
+            content: content,
+            attachment: attachment,
+            likes: 0,
+            UserId: userFound.id,
+          });
+          res.status(201).json({
+            message: message,
+            reponse: "Votre message a bien été posté !",
+          });
+        } else {
+          res.status(500).json({ message: "ça bloque" });
+        }
+      })
+      .catch((error) => {
+        return res.status(500).json({ error: error });
+      });
+  } catch (error)
+  {return res.status(500).send({ error: "Erreur serveur" });}
 };
+
+// Ajout du module pour récuperer l'ensemble des messages utilisateurs 
+
+exports.getMessages = (req,res,next)=>{
+
+
+    
+}
--- Backend/middleware/authmessage.js
+++ Backend/middleware/authmessage.js
@@ -5,7 +5,7 @@ function getUserId(req) {
     // on vérifie le userId du token
     const token = req.cookies.jwt; // on récupère le token de la requête entrante
     const decodedToken = JWT.verify(token, process.env.RANDOM_TOKEN_SECRET); // on le vérifie
-    const userId = decodedToken.sub;
+    const userId = decodedToken.id;
     return userId; // on récupère l'id du token
   };
 
